d70aa2044b8e61759146108f1dd1d48e702345fd
Ukpa Uchechi <ukpauchechi1@gmail.com>
2021-03-31 17:26:13 +0100

    first commit

--- javascript/question2.js
+++ javascript/question2.js
@@ -1,6 +1,7 @@
 // Complete the beautifulDays function below.
 function beautifulDays(i, j, k) {
   const array = []
+  const newarray = []
 
   for (let x = i; x <= j; x++) {
     console.log(x)
@@ -11,11 +12,23 @@ function beautifulDays(i, j, k) {
     const div = x - reverves
     console.log(div / 6)
     // if (parseInt(div / 6)) {
-    //     console.log(div/6)
+    newarray.push(div / 6)
     // }
-    return div / 6
+    // return div / 6
+
+    if ((div / 6).isInteger) {
+      console.log(div / 6)
+    }
     // array.push(x)
 
+  }
+  console.log(newarray)
+
+  for (let i = 0; i <= newarray; i++) {
+
+    if (newarray[i].isInteger) {
+      console.log(newarray[i])
+    }
   }
   // console.log(array)
   // return null;
