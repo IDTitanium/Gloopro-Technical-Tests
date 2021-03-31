1b88a38ffc1e5376664bbd87379831cceffdbd1f
Ukpa Uchechi <ukpauchechi1@gmail.com>
2021-03-31 17:19:18 +0100

    first commit

--- javascript/question1.js
+++ javascript/question1.js
@@ -1,6 +1,29 @@
 // Complete the saveThePrisoner function below.
 function saveThePrisoner(n, m, s) {
-  return null;
-}
 
+  const array = []
+  // const newarray = []
+  // // console.log(s)
+  for (let i = 1; i <= n; i++) {
+    array.push(i)
+    // console.log(m)
+  }
+
+  if (n > m) {
+    let newarray = array.slice(s, m)
+    console.log(newarray)
+
+    return newarray[newarray.length - 1]
+  }
+  else if (m > n) {
+    let newarray = array.slice(s, m)
+    // console.log(newarray)
+    console.log(m % n)
+    console.log(array)
+    return array[m % n]
+  }
+
+
+
+}
 module.exports = saveThePrisoner
--- javascript/question2.js
+++ javascript/question2.js
@@ -1,6 +1,27 @@
 // Complete the beautifulDays function below.
 function beautifulDays(i, j, k) {
-  return null;
+  const array = []
+
+  for (let x = i; x <= j; x++) {
+    console.log(x)
+    // x - 
+    const reverves = x.toFixed(0).split("").reverse().join("")
+
+    console.log(x - reverves)
+    const div = x - reverves
+    console.log(div / 6)
+    // if (parseInt(div / 6)) {
+    //     console.log(div/6)
+    // }
+    return div / 6
+    // array.push(x)
+
+  }
+  // console.log(array)
+  // return null;
+  // for ()
 }
+
+beautifulDays(20, 23, 6)
 
 module.exports = beautifulDays
